0a2e5994dc64af72da5e09136ec4129c97cef0f1
matteoFagundez <matteofagundez@gmail.com>
2026-01-16 19:14:56 -0300

    init

---
+++ wedding-app/src/App.tsx
@@ -0,0 +1,27 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <p className="read-the-docs">
+        Working....
+      </p>
+      
+    </>
+  )
+}
+
+export default App
